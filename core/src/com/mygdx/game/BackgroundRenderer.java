830b1ddf73abcce8b2cfd1de812c20f1481d1e86
jewjewthanawat <g-o.v-nus@hotmail.com>
2016-11-03 21:34:49 +0700

    ver 0.2.1 resize area

--- core/src/com/mygdx/game/BackgroundRenderer.java
+++ core/src/com/mygdx/game/BackgroundRenderer.java
@@ -22,7 +22,7 @@ public class BackgroundRenderer {
 	private TextureRegion storageCard;
 	private TextureRegion submitButton;
 	private TextureRegion passButton;
-	private static float size = 0.5f;
+	
 	public BackgroundRenderer(SlaveCardGame slaveCardGame , World world) {
 		this.slaveCardGame = slaveCardGame;
 		this.world = world;
@@ -46,21 +46,21 @@ public class BackgroundRenderer {
 	public void render(float delta) {
 		SpriteBatch batch = slaveCardGame.batch;
 		batch.begin();
-		batch.draw(field, 640*size, 440*size, 0, 0, 320*size, 320*size, 1, 1, 0);
-		batch.draw(cardBar, 280*size, 100*size, 0, 0, 1040*size, 100*size, 1, 1, 0);
-		batch.draw(choosedCardBar, 640*size, 250*size, 0, 0, 320*size, 100*size, 1, 1, 0);
-		batch.draw(botCardBar1, 100*size, 250*size, 0, 0, 100*size, 700*size, 1, 1, 0);
-		batch.draw(botChoosedCardBar1, 250*size, 440*size, 0, 0, 100*size, 320*size, 1, 1, 0);
-		batch.draw(botCardBar2, 450*size, 1000*size, 0, 0, 700*size, 100*size, 1, 1, 0);
-		batch.draw(botChoosedCardBar2, 640*size, 850*size, 0, 0, 320*size, 100*size, 1, 1, 0);
-		batch.draw(botCardBar3, 1400*size, 250*size, 0, 0, 100*size, 700*size, 1, 1, 0);
-		batch.draw(botChoosedCardBar3, 1250*size, 440*size, 0, 0, 100*size, 320*size, 1, 1, 0);
-		batch.draw(clockWise, 100*size, 1000*size, 0, 0, 100*size, 100*size, 1, 1, 0);
-		batch.draw(round, 250*size, 1000*size, 0, 0, 100*size, 100*size, 1, 1, 0);
-		batch.draw(score, 1400*size, 1000*size, 0, 0, 100*size, 100*size, 1, 1, 0);
-		batch.draw(storageCard, 110*size, 100*size, 0, 0, 80*size, 100*size, 1, 1, 0);
-		batch.draw(submitButton, 1400*size, 170*size, 0, 0, 100*size, 60*size, 1, 1, 0);
-		batch.draw(passButton, 1400*size, 100*size, 0, 0, 100*size, 60*size, 1, 1, 0);
+		batch.draw(field, 320, 220, 0, 0, 160, 160, 1, 1, 0);
+		batch.draw(cardBar, 140, 50, 0, 0, 520, 50, 1, 1, 0);
+		batch.draw(choosedCardBar, 320, 125, 0, 0, 160, 50, 1, 1, 0);
+		batch.draw(botCardBar1, 50, 125, 0, 0, 50, 350, 1, 1, 0);
+		batch.draw(botChoosedCardBar1, 125, 220, 0, 0, 50, 160, 1, 1, 0);
+		batch.draw(botCardBar2, 225, 500, 0, 0, 350, 50, 1, 1, 0);
+		batch.draw(botChoosedCardBar2, 320, 425, 0, 0, 160, 50, 1, 1, 0);
+		batch.draw(botCardBar3, 700, 125, 0, 0, 50, 350, 1, 1, 0);
+		batch.draw(botChoosedCardBar3, 625, 220, 0, 0, 50, 160, 1, 1, 0);
+		batch.draw(clockWise, 50, 500, 0, 0, 50, 50, 1, 1, 0);
+		batch.draw(round, 125, 500, 0, 0, 50, 50, 1, 1, 0);
+		batch.draw(score, 700, 500, 0, 0, 50, 50, 1, 1, 0);
+		batch.draw(storageCard, 55, 50, 0, 0, 40, 50, 1, 1, 0);
+		batch.draw(submitButton, 700, 87, 0, 0, 50, 30, 1, 1, 0);
+		batch.draw(passButton, 700, 50, 0, 0, 50, 30, 1, 1, 0);
 		batch.end();
 	}
 }
--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -5,11 +5,10 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.mygdx.game.SlaveCardGame;
 
 public class DesktopLauncher {
-	static double size = 0.5;
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = (int) (1600*size);
-		config.height = (int) (1200*size);
+		config.width = 800;
+		config.height = 600;
 		new LwjglApplication(new SlaveCardGame(), config);
 	}
 }
